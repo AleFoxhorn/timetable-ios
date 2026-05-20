a058f98aae969fcbba7131b5f7600d7a77544f7c
ALE <aileenchang@mail.dlut.edu.cn>
2026-05-20 15:26:23 +0800

    Initial Commit

---
+++ timetable/timetableApp.swift
@@ -0,0 +1,17 @@
+//
+//  timetableApp.swift
+//  timetable
+//
+//  Created by 张星 on 2026/5/20.
+//
+
+import SwiftUI
+
+@main
+struct timetableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
